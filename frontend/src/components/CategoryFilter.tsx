5a4b6ac6cf1bf6afab0cf9c4b0c5b17325d345ac
J0S3PH12345 <josephtew9020@gmail.com>
2025-04-04 15:58:33 -0600

    Fixed typo in Fixing Categories

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -15,7 +15,7 @@ function CategoryFilter({
     useEffect(() =>{
         const fetchCategories = async () => {
             try{
-            const response = await fetch('https://bookstoreproject-tew-backend-bwhebscrf9h3d5ge.eastus-01.azurewebsites.net/api/BookBook/GetBookCategories');
+            const response = await fetch('https://bookstoreproject-tew-backend-bwhebscrf9h3d5ge.eastus-01.azurewebsites.net/api/Book/GetBookCategories');
             const data = await response.json();
             console.log('Fetched categories:', data);
             setCategories(data)
